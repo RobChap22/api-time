546cc215524fc78ce0f5705cf025b62c8dab759a
Rob Chapman <autosexual@gmail.com>
2020-10-29 15:42:58 +0900

    api up and running

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <h1>{{ post.fields.title }}</h1>
+    <img :src='post.fields.image.fields.file.url' >
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  import Vue from 'vue';
+  import * as Contentful from 'contentful';
 
-export default Vue.extend({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-});
+
+  export default Vue.extend({
+    name: 'Home',
+    data() {
+      return {
+        client: null,
+        post: null,
+      }
+    },
+    mounted() {
+      this.client = Contentful.createClient({
+          space: "g31dyjc0dhaa",
+          accessToken: "RTomXH7fKYbwdsci7vLV4VThsANN4Fv_PydbBNtfueA"
+        })
+      console.log(this.client);
+      this.client
+        .getEntry("1ZHnNklxa1ObDT5CdHOghe")
+        .then(entry => this.post = entry)
+        .catch(err => console.log(err));
+    },
+  });
 </script>
